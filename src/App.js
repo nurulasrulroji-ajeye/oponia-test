df3a4330391d0ac229a51a53292f44d905efd7d0
Nurul Arul Roji <107383033+nurulasrulroji-ajeye@users.noreply.github.com>
2022-08-01 18:56:18 +0700

    thrid

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Content from "./components/Content/Content";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Content />
+    </>
+  );
+}
+
+export default App;
